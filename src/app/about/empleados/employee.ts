be03d6ae69ab4c1d38ba07cc14fb3398826848c6
nestdanchia <ndchiariello@gmail.com>
2022-01-08 15:48:59 -0300

    mat-card

--- src/app/about/empleados/employee.ts
+++ src/app/about/empleados/employee.ts
@@ -3,3 +3,37 @@ export class Employee {
 	age: number | null = null;
 	skill = null;
 }
+/*
+.ng-valid[required], .ng-valid.required  {
+  border-left: 2px solid #42A948; /* green 
+}
+
+.ng-invalid:not(form)  {
+  border-left: 2px solid #dd1612; /* red *
+}
+
+.alert div {
+  background-color: #fed3d3;
+  color: #820000;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+.form-group {
+  margin-bottom: 1rem;
+}
+
+label {
+  display: block;
+  margin-bottom: .5rem;
+}
+
+  
+  select {
+    width: 100%;
+    padding: .5rem;
+  }
+
+
+
+*/
